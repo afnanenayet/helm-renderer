8a7eaf4112bdca24d04ba362de776db778691e50
Afnan Enayet <afnan@blend.com>
2020-03-24 18:35:18 -0400

    Use binaries instead of building from scratch
    Update the homebrew formula to download binaries from github

--- HomebrewFormula/helm-renderer.rb
+++ HomebrewFormula/helm-renderer.rb
@@ -2,9 +2,9 @@ require "language/haskell"
 
 class HelmRenderer < Formula
   include Language::Haskell::Cabal
-  version "1.0.0"
+  version "0.1.4.1"
   desc "Render Helm charts for your k8s cluster locally"
-  url "https://github.com/afnanenayet/helm-renderer/releases/download/v0.1.4.1/helm-renderer-darwin"
+  url "https://github.com/afnanenayet/helm-renderer/releases/download/v#{version}/helm-renderer-darwin"
   homepage "https://github.com/afnan/helm-renderer"
   head "https://github.com/afnan/helm-renderer.git"
 
@@ -12,13 +12,18 @@ class HelmRenderer < Formula
   depends_on "cabal-install" => :build
   depends_on "ghc" => :build
 
-  bottle do
-    mv "helm-renderer-darwin" "helm-renderer"
+  if OS.mac?
+    url "https://github.com/afnanenayet/helm-renderer/releases/download/v#{version}/helm-renderer-darwin.tar.gz"
+    sha256 "92635e29711da0910f08235ea93adbccc38c1bfbe9c35a050cfd738e845bc808"
+  elsif OS.linux?
+    url "https://github.com/afnanenayet/helm-renderer/releases/download/v#{version}/helm-renderer-linux.tar.gz"
   end
 
   def install
-    cabal_sandbox do
-      install_cabal_package
-    end
+    bin.install "helm-renderer"
+  end
+
+  test do
+    shell_output "#{bin}/helm-renderer --help"
   end
 end
