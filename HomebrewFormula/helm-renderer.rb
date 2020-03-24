5f43170879a28291044400337bcf414b67717a3c
Afnan Enayet <afnan@blend.com>
2020-03-24 18:25:03 -0400

    Update homebrew formula

--- HomebrewFormula/helm-renderer.rb
+++ HomebrewFormula/helm-renderer.rb
@@ -4,12 +4,17 @@ class HelmRenderer < Formula
   include Language::Haskell::Cabal
   version "1.0.0"
   desc "Render Helm charts for your k8s cluster locally"
-  homepage "https://git.blendlabs.com/blend/helm-renderer"
-  head "https://git.blendlabs.com/blend/helm-renderer.git"
+  url "https://github.com/afnanenayet/helm-renderer/releases/download/v0.1.4.1/helm-renderer-darwin"
+  homepage "https://github.com/afnan/helm-renderer"
+  head "https://github.com/afnan/helm-renderer.git"
 
   depends_on "stack" => :build
   depends_on "cabal-install" => :build
   depends_on "ghc" => :build
+
+  bottle do
+    mv "helm-renderer-darwin" "helm-renderer"
+  end
 
   def install
     cabal_sandbox do
