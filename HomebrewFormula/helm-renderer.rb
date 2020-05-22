25ee98085b1367077bd4c5debe8093820504d293
Afnan Enayet <afnan@afnan.io>
2020-05-21 22:21:04 -0400

    Update homebrew formula for new version

--- HomebrewFormula/helm-renderer.rb
+++ HomebrewFormula/helm-renderer.rb
@@ -2,22 +2,13 @@ require "language/haskell"
 
 class HelmRenderer < Formula
   include Language::Haskell::Cabal
-  version "0.1.4.1"
+  version "0.1.4.2"
   desc "Render Helm charts for your k8s cluster locally"
-  url "https://github.com/afnanenayet/helm-renderer/releases/download/v#{version}/helm-renderer-darwin"
   homepage "https://github.com/afnan/helm-renderer"
   head "https://github.com/afnan/helm-renderer.git"
 
-  depends_on "stack" => :build
-  depends_on "cabal-install" => :build
-  depends_on "ghc" => :build
-
-  if OS.mac?
-    url "https://github.com/afnanenayet/helm-renderer/releases/download/v#{version}/helm-renderer-darwin.tar.gz"
-    sha256 "92635e29711da0910f08235ea93adbccc38c1bfbe9c35a050cfd738e845bc808"
-  elsif OS.linux?
-    url "https://github.com/afnanenayet/helm-renderer/releases/download/v#{version}/helm-renderer-linux.tar.gz"
-  end
+  url "https://github.com/afnanenayet/helm-renderer/releases/download/v#{version}/helm-renderer-darwin.tar.gz"
+  sha256 "75109e113dd0da5e8dca314610345d51ba912b7cc0130a76a63fdbd048156eed"
 
   def install
     bin.install "helm-renderer"
