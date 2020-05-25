6d1582f3233c45f645810bc11b53141bd84c7ea0
Afnan Enayet <afnan@afnan.io>
2020-05-25 16:51:11 -0400

    CHange name of completion files

--- generate_completion_script.sh
+++ generate_completion_script.sh
@@ -3,8 +3,8 @@
 # A script to generate the completions for the executable
 mkdir -p completions
 
-completion_script_filename="helm-renderer-completions"
 exe_name="helm-renderer"
+completion_script_filename="${exe_name}"
 
 stack run -- --bash-completion-script "${exe_name}" > completions/"${completion_script_filename}".bash 
 stack run -- --zsh-completion-script "${exe_name}" > completions/"${completion_script_filename}".zsh 
