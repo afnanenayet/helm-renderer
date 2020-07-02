5e8b0f1c17e387c69aa6940bf30c80939dcca2a0
Afnan Enayet <afnan@afnan.io>
2020-07-02 03:04:19 -0400

    Format files with `ormolu`

--- app/Main.hs
+++ app/Main.hs
@@ -2,63 +2,65 @@
 
 module Main where
 
-
-import           Lib
-import qualified Data.Text                     as T
-import qualified Data.Text.IO                  as TIO
-import           Options.Applicative
-import           Data.Semigroup                 ( (<>) )
-import           System.FilePath
-import           Turtle
-import           Data.Maybe
-import           System.Exit
+import Data.Maybe
+import Data.Semigroup ((<>))
+import qualified Data.Text as T
+import qualified Data.Text.IO as TIO
+import Lib
+import Options.Applicative
+import System.Exit
+import System.FilePath
+import Turtle
 
 optParser :: Parser Args
 optParser =
-    Args
-        <$> strArgument
-                (metavar "CHARTNAME" <> help
-                    "The name of the chart in the form 'repo/chartname'"
-                )
-        <*> optional
-                (strOption
-                    (  long "namespace"
-                    <> short 'n'
-                    <> metavar "NAMESPACE"
-                    <> help "The namespace to pass to Helm"
-                    )
-                )
-        <*> optional
-                (strOption
-                    (long "values" <> short 'f' <> metavar "VALUES" <> help
-                        "The path to the optional values YAML"
-                    )
-                )
-        <*> optional
-                (strOption
-                    (long "out" <> metavar "OUTDIR" <> help
-                        "The output directory to write the generated files to"
-                    )
-                )
+  Args
+    <$> strArgument
+      ( metavar "CHARTNAME"
+          <> help
+            "The name of the chart in the form 'repo/chartname'"
+      )
+    <*> optional
+      ( strOption
+          ( long "namespace"
+              <> short 'n'
+              <> metavar "NAMESPACE"
+              <> help "The namespace to pass to Helm"
+          )
+      )
+    <*> optional
+      ( strOption
+          ( long "values" <> short 'f' <> metavar "VALUES"
+              <> help
+                "The path to the optional values YAML"
+          )
+      )
+    <*> optional
+      ( strOption
+          ( long "out" <> metavar "OUTDIR"
+              <> help
+                "The output directory to write the generated files to"
+          )
+      )
 
 main :: IO ()
 main = do
-    args <- execParser opts
-    let cmd = helmCommand args
-    (code, out) <- shellStrict (T.pack cmd) empty
-    if code == ExitSuccess
-        then do
-            putStrLn "Retrieved Helm output successfully"
-            let preprocessed = preprocess out
-            let structs      = (catMaybes . generateStruct) preprocessed
-            processStructs (outputDir args) structs
-            exitSuccess
-        else do
-            TIO.putStr out
-            exitWith code
+  args <- execParser opts
+  let cmd = helmCommand args
+  (code, out) <- shellStrict (T.pack cmd) empty
+  if code == ExitSuccess
+    then do
+      putStrLn "Retrieved Helm output successfully"
+      let preprocessed = preprocess out
+      let structs = (catMaybes . generateStruct) preprocessed
+      processStructs (outputDir args) structs
+      exitSuccess
+    else do
+      TIO.putStr out
+      exitWith code
   where
     opts =
-        info (optParser <**> helper)
-            $  fullDesc
-            <> progDesc "Render Helm charts"
-            <> Options.Applicative.header "helm-renderer"
+      info (optParser <**> helper) $
+        fullDesc
+          <> progDesc "Render Helm charts"
+          <> Options.Applicative.header "helm-renderer"
--- src/Lib.hs
+++ src/Lib.hs
@@ -2,42 +2,44 @@
 {-# LANGUAGE StrictData #-}
 
 module Lib
-    ( preprocess
-    , splitYamls
-    , getTemplatePath
-    , helmCommand
-    , generateStruct
-    , processStructs
-    , outputDir
-    , Args(Args)
-    )
+  ( preprocess,
+    splitYamls,
+    getTemplatePath,
+    helmCommand,
+    generateStruct,
+    processStructs,
+    outputDir,
+    Args (Args),
+  )
 where
 
-import qualified Data.Text                     as T
-import qualified Data.Text.IO                  as TIO
-import           Data.List
-import           Data.List.Split
-import           System.FilePath               as FP
-import qualified Data.Maybe.Strict             as S
-import           Turtle
-import           Data.Maybe
+import Data.List
+import Data.List.Split
+import Data.Maybe
+import qualified Data.Maybe.Strict as S
+import qualified Data.Text as T
+import qualified Data.Text.IO as TIO
+import System.FilePath as FP
+import Turtle
 
--- |The command line arguments to use for the program
+-- | The command line arguments to use for the program
 data Args = Args
-    { chartName :: String
-    , namespace :: Maybe String
-    , valueFile :: Maybe String
-    , outputDir :: Maybe String
-    } deriving Show
+  { chartName :: String,
+    namespace :: Maybe String,
+    valueFile :: Maybe String,
+    outputDir :: Maybe String
+  }
+  deriving (Show)
 
--- |A struct containing the contents of a YAML file and its metadata
+-- | A struct containing the contents of a YAML file and its metadata
 data Yaml = Yaml
-    { yamlFilePath :: String -- File path of the YAML file
-    , fileContents :: T.Text -- The text contents of the file
-    } deriving (Show, Eq)
+  { yamlFilePath :: String, -- File path of the YAML file
+    fileContents :: T.Text -- The text contents of the file
+  }
+  deriving (Show, Eq)
 
--- |The delimiter marking the difference between sections in combined YAML
--- files
+-- | The delimiter marking the difference between sections in combined YAML
+--  files
 yamlFileDelimiter :: T.Text
 yamlFileDelimiter = "---"
 
@@ -47,70 +49,70 @@ yamlFileDelimiter = "---"
 helmReleaseName :: String
 helmReleaseName = "generated"
 
--- |The Helm chart requests a release name that it adds to every name, we use
--- this identifier and delete it in the output files.
+-- | The Helm chart requests a release name that it adds to every name, we use
+--  this identifier and delete it in the output files.
 generatedReleasePrefix :: T.Text
 generatedReleasePrefix = mconcat [T.pack helmReleaseName, "-"]
 
--- |Identifies whether a field is a debug field from Helm that isn't part of a
--- valid k8s spec
+-- | Identifies whether a field is a debug field from Helm that isn't part of a
+--  valid k8s spec
 isDebugField :: T.Text -> Bool
-isDebugField "NAME"          = True
+isDebugField "NAME" = True
 isDebugField "LAST DEPLOYED" = True
-isDebugField "NAMESPACE"     = True
-isDebugField "STATUS"        = True
-isDebugField "REVISION"      = True
-isDebugField "TEST SUITE"    = True
-isDebugField "HOOKS"         = True
-isDebugField "MANIFEST"      = True
-isDebugField _               = False
+isDebugField "NAMESPACE" = True
+isDebugField "STATUS" = True
+isDebugField "REVISION" = True
+isDebugField "TEST SUITE" = True
+isDebugField "HOOKS" = True
+isDebugField "MANIFEST" = True
+isDebugField _ = False
 
 -- | The prefix in a rendered helm YAML file that indicates which template file
 -- a section was rendered from
 sourceCommentLeader :: T.Text
 sourceCommentLeader = "# Source:"
 
--- |Split a string that is comprised of multiple YAML files into separate YAML
--- files.
+-- | Split a string that is comprised of multiple YAML files into separate YAML
+--  files.
 --
--- Helm provides YAML files that are structured as such:
+--  Helm provides YAML files that are structured as such:
 --
--- ```yaml
--- ---
--- # YAML File 1
--- key: value
--- ---
--- # YAML File 2
--- key: value
--- ...
--- ```
+--  ```yaml
+--  ---
+--  # YAML File 1
+--  key: value
+--  ---
+--  # YAML File 2
+--  key: value
+--  ...
+--  ```
 --
--- This method splits the strings into a list of strings, where each string is
--- a separate yaml file.
+--  This method splits the strings into a list of strings, where each string is
+--  a separate yaml file.
 splitYamls :: T.Text -> [T.Text]
 splitYamls =
-    map T.strip
-        . filter T.null
-        . map T.unlines
-        . filter null -- remove empty chunks
-        . splitOn [yamlFileDelimiter]
-        . T.lines
+  map T.strip
+    . filter T.null
+    . map T.unlines
+    . filter null -- remove empty chunks
+    . splitOn [yamlFileDelimiter]
+    . T.lines
 
--- |Preprocess the shell output from Helm into a YAML file that can be
--- interpreted by Kubernetes. This will strip debug fields, empty lines, and
--- trailing whitespace.
+-- | Preprocess the shell output from Helm into a YAML file that can be
+--  interpreted by Kubernetes. This will strip debug fields, empty lines, and
+--  trailing whitespace.
 preprocess :: T.Text -> T.Text
 preprocess =
-    T.unlines
-        . stripEmptyLines
-        . withoutRelease
-        . withoutEndNote
-        . stripDebugFields
-        . T.lines
+  T.unlines
+    . stripEmptyLines
+    . withoutRelease
+    . withoutEndNote
+    . stripDebugFields
+    . T.lines
   where
     stripEmptyLines = filter (not . T.null)
-    withoutEndNote  = takeWhile (\x -> T.strip x /= "NOTES:")
-    withoutRelease  = map $ T.replace generatedReleasePrefix mempty
+    withoutEndNote = takeWhile (\x -> T.strip x /= "NOTES:")
+    withoutRelease = map $ T.replace generatedReleasePrefix mempty
 
 -- | Strip debug fields from a yaml file
 --
@@ -120,7 +122,8 @@ stripDebugFields :: [T.Text] -> [T.Text]
 -- Need a safe version for an empty list, since we're using `head` in the other
 -- branch
 stripDebugFields [] = []
-stripDebugFields xs = filter
+stripDebugFields xs =
+  filter
     (not . isDebugField . extractHead . safeHead . T.splitOn ":")
     xs
   where
@@ -129,125 +132,128 @@ stripDebugFields xs = filter
 
 -- | A safe replacement for `head`
 safeHead :: [a] -> Maybe a
-safeHead []      = Nothing
+safeHead [] = Nothing
 safeHead (x : _) = Just x
--- |A constant for the name of the `templates/` folder in Helm. This demarcates
--- the top-level directory for a set of rendered templates.
+
+-- | A constant for the name of the `templates/` folder in Helm. This demarcates
+--  the top-level directory for a set of rendered templates.
 templateFolderName :: String
 templateFolderName = "templates"
 
--- |Retrieve the name of the template that the chart. This is a fallible
--- operation, as there is no guarantee that the source comment will be present
--- in the YAML file. This also retrieves the directory of the given filepath,
--- getting the path from `/templates/`.
+-- | Retrieve the name of the template that the chart. This is a fallible
+--  operation, as there is no guarantee that the source comment will be present
+--  in the YAML file. This also retrieves the directory of the given filepath,
+--  getting the path from `/templates/`.
 getTemplatePath :: T.Text -> Maybe FP.FilePath
 getTemplatePath contents = do
-    filepath <- getTemplatePath' contents
-    let filename     = takeFileName filepath
-    let tokenizedDir = FP.splitDirectories filepath
-    let dir =
-            (joinPath . tail . dropWhile (templateFolderName /=)) tokenizedDir
-    return dir
+  filepath <- getTemplatePath' contents
+  let filename = takeFileName filepath
+  let tokenizedDir = FP.splitDirectories filepath
+  let dir =
+        (joinPath . tail . dropWhile (templateFolderName /=)) tokenizedDir
+  return dir
 
--- |Helper function to get the relative path of a template. This method
--- isolates the raw text demarcating the path.
+-- | Helper function to get the relative path of a template. This method
+--  isolates the raw text demarcating the path.
 getTemplatePath' :: T.Text -> Maybe FP.FilePath
 getTemplatePath' contents = do
-    let ls = T.lines contents
-    sourceLine <- Data.List.find (T.isPrefixOf sourceCommentLeader) ls
-    -- Given some string of the form "path/to/template.yaml", we take the last
-    -- "/" chunk, and strip the ".yaml" from it to get the filename.
-    filepath   <- T.unpack <$> T.stripPrefix sourceCommentLeader sourceLine
-    return $ makeValid filepath
+  let ls = T.lines contents
+  sourceLine <- Data.List.find (T.isPrefixOf sourceCommentLeader) ls
+  -- Given some string of the form "path/to/template.yaml", we take the last
+  -- "/" chunk, and strip the ".yaml" from it to get the filename.
+  filepath <- T.unpack <$> T.stripPrefix sourceCommentLeader sourceLine
+  return $ makeValid filepath
 
--- |Generate the Helm command to call to generate the YAML file
+-- | Generate the Helm command to call to generate the YAML file
 helmCommand :: Args -> String
 helmCommand args =
-    -- We *might* add the namespace and values command if they are provided
-    unwords $ ["helm", "template", helmReleaseName, chartName args] ++ catMaybes
+  -- We *might* add the namespace and values command if they are provided
+  unwords $
+    ["helm", "template", helmReleaseName, chartName args]
+      ++ catMaybes
         [namespaceCmd, valuesCmd]
   where
     namespaceCmd = namespaceCommand $ namespace args
-    valuesCmd    = valuesCommand $ valueFile args
+    valuesCmd = valuesCommand $ valueFile args
 
--- |Generate the portion of the Helm command that dictates which namespace to
--- use
+-- | Generate the portion of the Helm command that dictates which namespace to
+--  use
 namespaceCommand :: Maybe String -> Maybe String
 namespaceCommand = fmap $ addNamedFlag "--namespace"
 
--- |Generate the portion of the Helm command that dictates which values YAML
--- file to use
+-- | Generate the portion of the Helm command that dictates which values YAML
+--  file to use
 valuesCommand :: Maybe String -> Maybe String
 valuesCommand = fmap $ addNamedFlag "-f"
 
--- |Given the name of a flag and the value of a flag, create a string with both
--- values in the proper order.
+-- | Given the name of a flag and the value of a flag, create a string with both
+--  values in the proper order.
 addNamedFlag :: String -> String -> String
 addNamedFlag flagName flagValue = unwords [flagName, flagValue]
 
--- |Create YAML struct objects with metadata from the Helm output
+-- | Create YAML struct objects with metadata from the Helm output
 generateStruct :: Text -> [Maybe Yaml]
 generateStruct txt = map toStruct split
   where
     split = splitYamls txt
     toStruct :: T.Text -> Maybe Yaml
     toStruct x = do
-        fp <- getTemplatePath x
-        return (Yaml fp x)
+      fp <- getTemplatePath x
+      return (Yaml fp x)
 
--- |Write the yaml file, given its contents, path, and the parent path
+-- | Write the yaml file, given its contents, path, and the parent path
 saveYamlFile :: String -> Maybe String -> Yaml -> IO ()
 saveYamlFile indexPrefix parentDir f = do
-    let rawPath = fullSavePath parentDir (yamlFilePath f)
-    -- We rename the actual filename to add the index prefix, so "x.yaml" becomes "0001_x.yaml"
-    let path    = addPrefixToPath indexPrefix rawPath
-    TIO.putStrLn $ saveFileMessage path
-    mktree $ (decodeString . takeDirectory . encodeString) path
-    TIO.writeFile (encodeString path) (fileContents f)
+  let rawPath = fullSavePath parentDir (yamlFilePath f)
+  -- We rename the actual filename to add the index prefix, so "x.yaml" becomes "0001_x.yaml"
+  let path = addPrefixToPath indexPrefix rawPath
+  TIO.putStrLn $ saveFileMessage path
+  mktree $ (decodeString . takeDirectory . encodeString) path
+  TIO.writeFile (encodeString path) (fileContents f)
 
--- |Create a message to display to the user informing them that a file has been
--- saved. This will print an error message if the filepath can't be easily
--- converted to a human-readable format.
+-- | Create a message to display to the user informing them that a file has been
+--  saved. This will print an error message if the filepath can't be easily
+--  converted to a human-readable format.
 saveFileMessage :: Turtle.FilePath -> T.Text
 saveFileMessage = f . toText
   where
     f :: Either T.Text T.Text -> T.Text
-    f (Left  _   ) = "A file was saved but the filename could not be printed"
+    f (Left _) = "A file was saved but the filename could not be printed"
     f (Right text) = mconcat ["Saved ", text]
 
--- |Calculate the full save path of a YAML file given the config
+-- | Calculate the full save path of a YAML file given the config
 fullSavePath :: Maybe String -> String -> Turtle.FilePath
-fullSavePath Nothing       fp = decodeString fp
+fullSavePath Nothing fp = decodeString fp
 fullSavePath (Just parent) fp = decodeString parent <> decodeString fp
 
--- |Pad a string representation of numbers with leading zeros `padZeros 4 20 ==
--- "0020"`. This is necessary so that the Helm chart files are listed in order.
+-- | Pad a string representation of numbers with leading zeros `padZeros 4 20 ==
+--  "0020"`. This is necessary so that the Helm chart files are listed in order.
 indexFilePrefix :: Int -> Int -> String
 indexFilePrefix x width = mconcat [filePrefix, xStr, "_"]
   where
-    xStr             = show x
-    nWidth           = length xStr
+    xStr = show x
+    nWidth = length xStr
     numLeadingZeroes = max 0 width - nWidth
-    filePrefix       = replicate numLeadingZeroes '0'
+    filePrefix = replicate numLeadingZeroes '0'
 
--- |Add a prefix to a filename, given the whole path. This will only modify the
--- base filename. This also handles converting the type to `Turtle.FilePath`.
+-- | Add a prefix to a filename, given the whole path. This will only modify the
+--  base filename. This also handles converting the type to `Turtle.FilePath`.
 addPrefixToPath :: String -> Turtle.FilePath -> Turtle.FilePath
 addPrefixToPath prefix path = directoryOfPath <> prefixedFilename
   where
-    directoryOfPath  = directory path
+    directoryOfPath = directory path
     prefixedFilename = mconcat [decodeString prefix, filename path]
 
--- |Process each YAML struct and save them to a file with the index in the
--- filename so all of the files are processed in the correct order (if they're
--- processed in alphabetical order).
+-- | Process each YAML struct and save them to a file with the index in the
+--  filename so all of the files are processed in the correct order (if they're
+--  processed in alphabetical order).
 processStructs :: Maybe String -> [Yaml] -> IO ()
 processStructs out structs = do
-    let charWidth = (length . show . length) structs
-    let idxZipped = zip [0 ..] structs
-    mapM_
-        (\(index, yaml) -> do
-            let prefix = indexFilePrefix index charWidth
-            saveYamlFile prefix out yaml
-        )
-        idxZipped
+  let charWidth = (length . show . length) structs
+  let idxZipped = zip [0 ..] structs
+  mapM_
+    ( \(index, yaml) -> do
+        let prefix = indexFilePrefix index charWidth
+        saveYamlFile prefix out yaml
+    )
+    idxZipped
